f43a1d7f67725bcb6c167584253fbc9761d67950
Hareesh292 <vharishv959@gmail.com>
2025-01-23 00:03:57 +0530

    done

--- special.py
+++ special.py
@@ -1,26 +1,20 @@
-s='we have class on sunday'
-l=[]
-revl=[]
-res=''
-for val in s:
-    if val==' ':
-        l=[res]+l
-        res=''
-    else:
-        res=val+res
-l+=[res]
-print(l)
-for vv in l:
-    revl=[vv]+revl
-print(revl)
-ind=0
-rev=''
-while ind!=len(revl):
-    if ind!=len(revl)-1:
-        rev=rev+revl[ind]+' '
-    else:
-        rev+=revl[ind]
-    ind+=1
-print(rev)
-    
+num=5
+for val in range(num):
+    for vv in range(num):
+        if vv==0 or val==0 or vv==num-1 or val==num-1:
+            print('* ',end='')
+        else:
+            print('  ',end='')
+    print()
+print('----------hellow triangle pattern------------')
+num=5
+for val in range(num):
+    for vv in range(val+1):
+        if vv==0 or val==vv or val==num-1:
+            print('* ',end='')
+        else:
+            print('  ',end='')
+    print()
+print('---------------right traingle pattern-------------------')
+
 
